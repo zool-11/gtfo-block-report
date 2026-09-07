077a0738fef3a3dccc2ef9b6505c102e1484936d
zool-11 <13711365@qq.com>
2026-09-07 08:07:18 +0800

    Refactor Harmony patching for InvokeEvent method

--- .github/workflows/Plugin.cs
+++ .github/workflows/Plugin.cs
@@ -21,10 +21,17 @@ namespace BlockPlayerStatusReport
 
         public override void Load()
         {
+            Log.LogInfo($"[{PluginInfo.Name}] 已加载，拦截 PlayerStatusUpdate 网络事件");
             _harmony = new Harmony(PluginInfo.GUID);
-            MethodInfo target = AccessTools.Method(typeof(NetworkAPI), "InvokeEvent", new[] {typeof(string), typeof(object)});
-            HarmonyMethod prefix = new HarmonyMethod(typeof(Patch), nameof(Patch.Prefix));
-            _harmony.Patch(target, prefix);
+
+            MethodInfo targetMethod = AccessTools.Method(
+                typeof(NetworkAPI),
+                "InvokeEvent",
+                new[] { typeof(string), typeof(object) }
+            );
+
+            HarmonyMethod prefixPatch = new HarmonyMethod(typeof(Patch), nameof(Patch.Prefix));
+            _harmony.Patch(targetMethod, prefixPatch);
         }
     }
 
@@ -32,7 +39,8 @@ namespace BlockPlayerStatusReport
     {
         public static bool Prefix(string eventName, object data)
         {
-            if(eventName == "PlayerStatusUpdate")
+            // 返回 false = 阻止原函数执行；true = 放行事件
+            if (eventName == "PlayerStatusUpdate")
             {
                 return false;
             }
